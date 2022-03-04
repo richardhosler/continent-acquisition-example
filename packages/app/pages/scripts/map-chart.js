f732a128764f1274f1c4cb0b8f4708d749c27d96
Richard Hosler <kaltaugh@gmail.com>
2022-03-04 13:44:02 +0000

    feat: wallet connection with metamask

---
+++ packages/app/pages/scripts/map-chart.js
@@ -0,0 +1,46 @@
+import React, { memo } from "react";
+import {
+    ZoomableGroup,
+    ComposableMap,
+    Geographies,
+    Geography
+} from "react-simple-maps";
+
+const geoUrl =
+    "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+const MapChart = ({ setTooltipContent }) => {
+    return (
+        <>
+            <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
+                <ZoomableGroup>
+                    <Geographies geography={geoUrl}>
+                        {({ geographies }) =>
+                            geographies.map(geo => (
+                                <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    style={{
+                                        default: {
+                                            fill: "#D6D6DA",
+                                            outline: "none"
+                                        },
+                                        hover: {
+                                            fill: "#F53",
+                                            outline: "none"
+                                        },
+                                        pressed: {
+                                            fill: "#E42",
+                                            outline: "none"
+                                        }
+                                    }}
+                                />
+                            ))
+                        }
+                    </Geographies>
+                </ZoomableGroup>
+            </ComposableMap>
+        </>
+    );
+};
+
+export default memo(MapChart);
