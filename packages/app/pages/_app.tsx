be51a54cb713adc4ac2d0536f9813999ed18e601
Chrykal <kaltaugh@gmail.com>
2022-03-16 15:06:55 +0000

    Upload for change to linux

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,22 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
-import "regenerator-runtime/runtime";
+import { Provider as WalletProvider, chain, defaultChains } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { WalletLinkConnector } from 'wagmi/connectors/walletLink'
 
+// API key for Ethereum node
+// Two popular services are Infura (infura.io) and Alchemy (alchemy.com)
+const infuraId = process.env.INFURA_ID
+
+// Chains for connectors to support
+const chains = defaultChains
+
+// Set up connectors
+const connectors = ({ chainId }) => {
+  const rpcUrl =
+    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
+    chain.mainnet.rpcUrls[0]
+  return [
+    new InjectedConnector({
+      chains,
+      options: { shimDisconnect: true },
+    }),
+    new WalletConnectConnector({
+      options: {
+        infuraId,
+        qrcode: true,
+      },
+    }),
+    new WalletLinkConnector({
+      options: {
+        appName: 'My wagmi app',
+        jsonRpcUrl: `${rpcUrl}/${infuraId}`,
+      },
+    }),
+  ]
+}
 function MyApp({ Component, pageProps }) {
   const supportedChainIds = [80001, 4];
 
-  const connectors = {
-    injected: {},
-  };
-
   return (
-    <ThirdwebWeb3Provider
-      supportedChainIds={supportedChainIds}
-      connectors={connectors}
-    >
+    <WalletProvider autoConnect connectors={connectors}>
       <Component {...pageProps} />
-    </ThirdwebWeb3Provider>
+    </WalletProvider>
   );
 }
 
--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,34 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import type { NextPage } from 'next'
-// import Head from 'next/head'
-// import styles from '../styles/Home.module.css'
-import { ThirdwebWeb3Provider, useWeb3 } from "@3rdweb/hooks";
 import "regenerator-runtime/runtime";
 import MapChart from "./components/MapChart";
+import { useConnect } from "wagmi";
 
 const Home: NextPage = () => {
-
-  // Connector for MM
-  const { connectWallet, address, error } = useWeb3();
-  error ? console.log(error) : null;
-  // Import web3 lib
-  // Observe/watch for connect/login
-
+  const [{ data, error }, connect] = useConnect()
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-slate-100">
-      <div className="absolute top-10 right-10">
-        {address ? (
-          <p className="px-2 py-1 rounded-full bg-gray-200 hover:bg-gray-300 font-mono font-medium cursor-pointer duration-100">
-            {address}
-          </p>
-        ) : (
+      <div>
+        {data.connectors.map((connector) => (
           <button
-            className="px-4 py-2 rounded-md bg-purple-600 cursor-pointer hover:bg-purple-500 text-xl font-semibold duration-100 text-white"
-            onClick={() => connectWallet("injected")}
+            disabled={!connector.ready}
+            key={connector.id}
+            onClick={() => connect(connector)}
           >
-            Connect Wallet
+            {connector.name}
+            {!connector.ready && ' (unsupported)'}
           </button>
-        )}
+        ))}
+
+        {error && <div>{error?.message ?? 'Failed to connect'}</div>}
       </div>
       <div className="flex">
         <MapChart />
